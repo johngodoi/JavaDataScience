451d21fe69844b78cd18bd5e85e19a1d1af49628
John Godoi <john.godoi@gmail.com>
2016-06-13 15:34:31 -0300

    maven initial

---
+++ src/main/java/br/unifesp/henrique/john/research/datascience/experiments/App.java
@@ -0,0 +1,13 @@
+package br.unifesp.henrique.john.research.datascience.experiments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
